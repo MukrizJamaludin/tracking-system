d2aff164d0023a569ba4664e8bf2960613be7a96
Mukriz Jamaludin <30573067+MukrizJamaludin@users.noreply.github.com>
2017-08-07 13:21:45 +0800

    Initialize git repository

---
+++ Phone Gap/platforms/android/assets/www/hds/testjava.java
@@ -0,0 +1,12 @@
+public class MyClass
+{
+ public static void main(String[] args)
+ {
+ sayHello(name);
+ }
+
+ public static void sayHello(String name)
+ {
+ System.out.println("Hello" + name);
+ }
+}
---
+++ Website/addstudent.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+<title>Add Student</title>
+</head>
+<body>
+
+<form action="http://localhost/studentadded.php" method="post">
+
+	<b>Add a New Student</b>
+
+	<p>First Name:
+	<input type="test" name="first_name" size="30" value="" />
+	</p>
+
+	<p>Last Name:
+	<input type="test" name="first_name" size="30" value="" />
+	</p>
+
+	<p>Email:
+	<input type="test" name="first_name" size="30" value="" />
+	</p>
+
+	<p>Street:
+	<input type="test" name="first_name" size="30" value="" />
+	</p>
+
+	<p>City:
+	<input type="test" name="first_name" size="30" value="" />
+	</p>
+
+	<p>State (2 Characters):
+	<input type="test" name="first_name" size="30" value="" />
+	</p>
+
+	<p>Zip Code:
+	<input type="test" name="first_name" size="30" value="" />
+	</p>
+
+	<p>Phone Number:
+	<input type="test" name="first_name" size="30" value="" />
+	</p>
+
+	<p>Birth Date (YYYY-MM-DD):
+	<input type="test" name="first_name" size="30" value="" />
+	</p>
+
+	<p>Sex (M or F):
+	<input type="test" name="first_name" size="30" value="" />
+	</p>
+
+	<p>Lunch Cost:
+	<input type="test" name="first_name" size="30" value="" />
+	</p>
+
+	<p>
+
+		<input type="submit" name="submit" value="Send" />
+
+	</form>
+
+</body>
+</html>
+
+
